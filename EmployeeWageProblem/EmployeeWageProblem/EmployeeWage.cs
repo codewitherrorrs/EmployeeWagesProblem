088e94916af157f18bf28f0aa230cd257e01ca07
“codewitherrors” <codewitherrors1@gmail.com>
2023-06-23 13:49:36 +0530

    Add : Check Employee is present or not

---
+++ EmployeeWageProblem/EmployeeWageProblem/EmployeeWage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public class EmployeeWage
+    {
+        public static void Employee()
+        {
+            Random random = new Random();
+            int check = random.Next(2);
+            int fulltime = 1;
+
+            if (check == fulltime)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
---
+++ EmployeeWageProblem/EmployeeWageProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWageProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+             EmployeeWage.Employee();
+        }
+    }
+}
